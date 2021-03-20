5669c9758371b691241a0cea217a634557c1e29b
Hasaan638 <73753029+Hasaan638@users.noreply.github.com>
2021-03-20 16:03:43 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+
+function preload(){
+    
+}
+
+function setup(){
+   background(600,400);
+    
+}
+
+function draw(){
+
+    drop = new Drops (200,200,10,10);
+
+    drop.display();
+    
+}   
+
